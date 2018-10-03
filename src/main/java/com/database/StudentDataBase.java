be2be253be3059b85bfa892bf029b1ef03e149fc
Pooja <you@example.com>
2018-10-03 17:24:21 +0530

    hod detail

--- src/main/java/com/database/StudentDataBase.java
+++ src/main/java/com/database/StudentDataBase.java
@@ -3,13 +3,19 @@ package com.database;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.student.data.HodPojo;
 import com.student.data.StudentPojo;
 
 public class StudentDataBase {
-	
-	static Map<Integer, StudentPojo> dataInfo=new HashMap<>();
-	
+
+	static Map<Integer, StudentPojo> dataInfo = new HashMap<>();
+	static Map<Integer, HodPojo> hodMap = new HashMap<>();
+
 	public static Map<Integer, StudentPojo> getAllData() {
 		return dataInfo;
+	}
+
+	public static Map<Integer, HodPojo> getAllHodData() {
+		return hodMap;
 	}
 }
