9329d66b3120c51904c1e3e3b13d14f2198b2831
pooja <pooja.mittal30@gmail.com>
2018-12-06 22:59:45 +0530

    xml/json acc to client request

--- src/main/java/com/resource/StudentResouceWithSql.java
+++ src/main/java/com/resource/StudentResouceWithSql.java
@@ -1,7 +1,6 @@
 package com.resource;
 
 import java.util.List;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -18,21 +17,20 @@ public class StudentResouceWithSql {
 	StudentSqlService studentService = new StudentSqlService();
 
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	public List<Student> getAllStudent() {
 		return studentService.getAll();
 	}
 
 	@GET
 	@Path("/{studentId}")
-	@Produces(MediaType.APPLICATION_JSON)
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	public Student getParticularStudent(@PathParam("studentId") int studentId) {
 		return studentService.getById(studentId);
 	}
 
 	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	public Student createNewStudent(Student s1) {
 		studentService.create(s1);
 		return s1;
@@ -40,15 +38,14 @@ public class StudentResouceWithSql {
 
 	@DELETE
 	@Path("/{studentId}")
-	@Produces(MediaType.APPLICATION_JSON)
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	public void deleteStudent(@PathParam("studentId") int studentId) {
 		studentService.deleteById(studentId);
 	}
 
 	@PUT
 	@Path("/{updateStudent}")
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	public Student updateStudentInfo(Student updateStudent) {
 		studentService.update(updateStudent);
 		return updateStudent;
